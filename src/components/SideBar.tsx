8881b0c4315fb3dd3d9731b0d80caba2aeab97cd
ManasAgarwal330 <agarwalmanas212001@gmail.com>
2021-08-11 14:48:58 -0700

    changed sidebar and made it responsive

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,51 +1,77 @@
-import { Transition } from "@headlessui/react";
-import { FC, memo } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { FC, Fragment, memo } from "react";
 import { AiOutlineHome } from "react-icons/ai";
 import { BiChip, BiCube } from "react-icons/bi";
+import ListItem from "./ListItem/ListItem";
 
 interface Props {
   open: boolean;
   setOpen: (value: false) => void;
 }
 
-const SideBar: FC<Props> = ({ open, setOpen }) => {
+const SideBar: FC<Props> = ({ open }) => {
   return (
     <>
       <Transition.Root show={open}>
         <Transition.Child
-          enter="ease-in-out duration-500 transform transition-all"
+          enter="ease-in-out duration-1000 transform transition-all"
           enterFrom="-translate-x-full"
           enterTo="translate-x-0"
-          leave="ease-in-out duration-500 transform transition-all"
+          leave="ease-in-out duration-1000 transform transition-all"
           leaveFrom="translate-0"
           leaveTo="-translate-x-full"
         >
-          <div className="w-52 h-screen bg-grayLightest absolute left-0">
+          <div className="w-56 h-screen bg-grayLightest absolute left-0">
             <ul className="flex-col mt-9 px-2">
-              <li
-                className="flex items-center mb-2 w-full transition-all duration-500 
-              ease-in-out hover:bg-gray-300 hover:shadow-md py-2 rounded-md px-1 cursor-pointer"
-              >
-                <AiOutlineHome className="h-5 w-5 text-primaryDark mr-2" />
-                <span className="text-black text-lg font-display">
-                  Dashboard
-                </span>
-              </li>
-              <li className="flex items-center mb-2 w-full transition-all duration-500 
-              ease-in-out hover:bg-gray-300 hover:shadow-md py-2 rounded-md px-1">
-                <BiChip className="h-5 w-5 text-primaryDark mr-2 cursor-pointer" />
-                <span className="text-black text-lg font-display">Apps</span>
-              </li>
-              <li className="flex items-center mb-2 w-full transition-all duration-500 
-              ease-in-out hover:bg-gray-300 hover:shadow-md py-2 rounded-md px-1 cursor-pointer">
-                <BiCube className="h-5 w-5 text-primaryDark mr-2" />
-                <span className="text-black text-lg font-display">
-                  Components
-                </span>
-              </li>
+              <ListItem title="Dashboard" Icon={AiOutlineHome} isArrow={true} />
+              <ListItem title="Apps" Icon={BiChip} isArrow={true} />
+              <ListItem title="Component" Icon={BiCube} />
             </ul>
           </div>
         </Transition.Child>
+      </Transition.Root>
+    </>
+  );
+};
+
+export const SidebarSmall: FC<Props> = ({ open, setOpen }) => {
+  return (
+    <>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog open={open} onClose={setOpen}>
+          <Transition.Child
+            enter="ease-in-out duration-1000 transition-opacity"
+            enterFrom="opacity-0"
+            enterTo="opacity-30"
+            entered="opacity-30"
+            leave="ease-in-out duration-1000 transition-opacity"
+            leaveFrom="opacity-30"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="bg-gray-400 absolute inset-0 z-30" />
+          </Transition.Child>
+          <Transition.Child
+            as={Fragment}
+            enter="transform transition-all duration-500 ease-in-out"
+            enterFrom="-translate-x-full"
+            enterTo="translate-x-0"
+            leave="ease-in-out duration-1000 transform"
+            leaveFrom="translate-x-0"
+            leaveTo="-translate-x-full"
+          >
+            <div className="w-56 bg-grayLightest fixed left-0 top-0 bottom-0">
+              <ul className="flex-col mt-9 px-2">
+                <ListItem
+                  title="Dashboard"
+                  Icon={AiOutlineHome}
+                  isArrow={true}
+                />
+                <ListItem title="Apps" Icon={BiChip} isArrow={true} />
+                <ListItem title="Component" Icon={BiCube} />
+              </ul>
+            </div>
+          </Transition.Child>
+        </Dialog>
       </Transition.Root>
     </>
   );
--- src/pages/AppContainer.page.tsx
+++ src/pages/AppContainer.page.tsx
@@ -5,34 +5,56 @@ import RecordingsPage from "./Recordings.page";
 import SideBar from "../components/SideBar";
 import Navbar from "../components/Navbar/Navbar";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { SidebarSmall } from "../components/SideBar";
 
 interface Props {}
 
 const AppContainer: FC<Props> = (props) => {
   const [openSidebar, setOpenSidebar] = useState(false);
+  const [openSmallSidebar, setOpenSmallSidebar] = useState(false);
   return (
     <>
-      <div className="">
+      <div className="overflow-hidden">
         <Navbar />
-        <div className="h-12 flex items-center w-full shadow-md pl-2">
+        <div className="h-12 flex items-center w-full shadow-md pl-2 mt-12 lg:hidden">
+          <button onClick={() => setOpenSmallSidebar((state) => !state)}>
+            <GiHamburgerMenu className="h-6 w-6" />
+          </button>
+        </div>
+        <div className="h-12 items-center w-full shadow-md pl-2 mt-12 hidden lg:flex">
           <button onClick={() => setOpenSidebar((state) => !state)}>
             <GiHamburgerMenu className="h-6 w-6" />
           </button>
         </div>
-        <SideBar open={openSidebar} setOpen={setOpenSidebar} />
-        <div
-          className={`h-screen flex transform transition-all duration-700 ease-in-out ${
-            openSidebar ? "translate-x-52" : "translate-x-0"
-          }`}
-        >
-          <Switch>
-            <Route path="/dashboard">
-              <DashboardPage />
-            </Route>
-            <Route path="/recordings/:batchNumber/:lectureNumber">
-              <RecordingsPage />
-            </Route>
-          </Switch>
+        <div className="block lg:hidden">
+          <SidebarSmall open={openSmallSidebar} setOpen={setOpenSmallSidebar} />
+          <div className={`h-screen flex`}>
+            <Switch>
+              <Route path="/dashboard">
+                <DashboardPage />
+              </Route>
+              <Route path="/recordings/:batchNumber/:lectureNumber">
+                <RecordingsPage />
+              </Route>
+            </Switch>
+          </div>
+        </div>
+        <div className="hidden lg:block">
+          <SideBar open={openSidebar} setOpen={setOpenSidebar} />
+          <div
+            className={`h-screen transform transition-all duration-1000 ease-in-out flex ${
+              openSidebar ? "translate-x-56" : "translate-x-0"
+            }`}
+          >
+            <Switch>
+              <Route path="/dashboard">
+                <DashboardPage />
+              </Route>
+              <Route path="/recordings/:batchNumber/:lectureNumber">
+                <RecordingsPage />
+              </Route>
+            </Switch>
+          </div>
         </div>
       </div>
     </>
