60e97e29b56207e8991c14ad188e2e7ec087d49f
ManasAgarwal330 <agarwalmanas212001@gmail.com>
2021-08-24 19:44:38 -0700

    added typings.d.ts file for redux dev tools

--- src/store.ts
+++ src/store.ts
@@ -5,29 +5,38 @@ import { User } from "./modals/User";
 
 export const ME_LOGIN = "me/login";
 
-interface AppState{
-    me ?: User;
-    groups:Groups[];
-    isSidebarOpen:boolean;
+interface AppState {
+  me?: User;
+  groups: Groups[];
+  isSidebarOpen: boolean;
 }
 
-const initialState:AppState = {
-    me:undefined,
-    groups:[],
-    isSidebarOpen:true,
-} 
+const initialState: AppState = {
+  me: undefined,
+  groups: [],
+  isSidebarOpen: true,
+};
 
-const reducer:Reducer<AppState> = (currentState = initialState,dispatchedAction:AnyAction) => {
-    switch(dispatchedAction.type){
-        case ME_LOGIN:
-            return {...currentState,me:dispatchedAction.payload};
-        default:
-            return currentState;
-    }
-}
+const reducer: Reducer<AppState> = (
+  currentState = initialState,
+  dispatchedAction: AnyAction
+) => {
+  switch (dispatchedAction.type) {
+    case ME_LOGIN:
+      return { ...currentState, me: dispatchedAction.payload };
+    default:
+      return currentState;
+  }
+};
 
-export const store = createStore(reducer); //eslint/no-unused-vars
+export const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+); //eslint/no-unused-vars
 
-export const meFetchAction = (user:User) => ({type:ME_LOGIN,payload:user});
+export const meFetchAction = (user: User) => ({
+  type: ME_LOGIN,
+  payload: user,
+});
 
 export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
